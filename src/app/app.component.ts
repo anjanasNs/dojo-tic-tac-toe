f33942d7779116839c671ddb9cdc629158dfe877
Anja van Hagen <anja.vanhagen@ns.nl>
2024-01-29 11:32:35 +0100

    Clean up default Angular stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'dojo-tic-tac-toe';
-}
+export class AppComponent {}
